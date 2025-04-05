828a5049d8acdd887678c6fd7a26050c0af6021b
wonpark1 <2023111927@dgu.ac.kr>
2025-04-05 16:45:10 +0900

    second hw

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mainpage, name='mainpage'),        # 첫 페이지
+    path('secondpage/', views.secondpage, name='secondpage')
 ]
